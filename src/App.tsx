5fe7cfae80eb888f199991ab5139fe5b435b133d
Kirill Tatarenko <Kirilltatarenko97@gmail.com>
2026-09-23 17:11:22 +0300

    first comit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {Card, Typography} from '@mui/material'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Card>
+      <Typography variant='h1'>в разработке</Typography>
+    </Card>
+    </>
+  )
+}
+
+export default App
